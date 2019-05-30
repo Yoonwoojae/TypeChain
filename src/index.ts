f47b4fb3852f623717a7a037a52a694d7f570612
yoonwoojae <dbsdnwp1004@naver.com>
2019-05-30 09:43:49 +0900

    1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export {};
 const name = "woojae",
-age = 34,
+age = 22,
 gender = "male"
 
-const sayHi = (name, age, gender) => {
+const sayHi = (name: String, age: number, gender: String): void => {
     console.log(` Hello ${name}, you are ${age}, you are a ${gender}`);
 }
 
